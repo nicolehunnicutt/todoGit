3f5cf64ac3f1330860c76670ef312f8bc7b48666
nicolehunnicutt <nicolehunnicutt@students.abtech.edu>
2014-09-25 18:36:29 -0400

    To Do Chapter 5 Assignment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todo::Application.config.secret_token = '90c590db48afdc9668be35d4ccc859437f65bc64e116dc91732469f9502b72f6aac813fe37efe5f821accd8828caa109fe8269e8b177a647b84c9496c62f01af'
---
+++ config/initializers/time_formats.rb
@@ -0,0 +1,2 @@
+Date::DATE_FORMATS[:default]="%Y/%m/%d"
+Time::DATE_FORMATS[:default]="%m/%d/%Y %I:%M %p"
